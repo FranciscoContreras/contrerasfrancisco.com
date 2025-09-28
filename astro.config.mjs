1eb0bcbde3730b3cc9e9a3166728a556ba718652
FranciscoContreras <Contrerasfrancisco2010@gmail.com>
2025-09-28 14:02:06 -0500

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://contrerasfrancisco.com',
-  output: 'hybrid',
-  adapter: node({ mode: 'standalone' }),
+  output: 'static',
 });
